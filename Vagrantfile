52a070e4951f81e502ef15a6334ad2cd086ea93d
Ivan Storck <ivanoats@gmail.com>
2013-06-18 15:29:44 -0700

    eager loading secure

--- Vagrantfile
+++ Vagrantfile
@@ -80,6 +80,8 @@ Vagrant.configure("2") do |config|
     chef.add_recipe "build-essential"
     chef.add_recipe "apache2"
     chef.add_recipe "postgresql"
+    chef.add_recipe "rvm"
+    chef.add_recipe "rvm_passenger"
   end
   # config.vm.provision :chef_solo do |chef|
   #   chef.cookbooks_path = "../my-recipes/cookbooks"
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,17 @@
 class Post < ActiveRecord::Base
   attr_accessible :body, :title
   has_many :comments
+
+  def self.search(search)
+    if search
+      #safe
+      #find(:all, :conditions => ['title like ?', "%#{search}%"])
+      includes(comments: :replies).where("title like ?", "%#{search}%")
+      #
+      #unsafe - SQL injection
+      #includes(comments: :replies).where("title like '%#{search}%'")
+    else
+      includes(comments: :replies)
+    end
+  end
 end
