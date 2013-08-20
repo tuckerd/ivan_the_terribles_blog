0480911c8c4ee8deae3ce527d6598efeffcd4a02
D Tucker <danielle@tuckerlabs.com>
2013-08-19 17:53:31 -0700

    Hide Comments link working

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,5 +1,10 @@
-$('.hide').click(function(){
+$(document).ready(function() {
+  $('a.hide').click( function(event) {
+  event.preventDefault();
+  //alert("yo i got clicked");
+  //console.log(this);
   $(this).hide();
-  //$(this).parent().find('#show_comments').show();
-  //$(this).parent().parent().find('.comment').empty();
+  $(this).parent().find('#show_comments').show();
+  $(this).parent().parent().find('.comment').empty();
   });
+});
